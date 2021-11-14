e5e2f91c43181084034ff3c6dd67f80a9a697282
JsExplorer <93700857+JsExplorer@users.noreply.github.com>
2021-11-14 22:10:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var totalamount = interest + principal
 
-    document.getElementById("result").innerHTML="If you deposit "+"<span class='tagged'>"+principal+"</span>"+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+totalamount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+"<span class='tagged'>"+principal+"</span>"+",\<br\>at an interest rate of "+"<span class='tagged'>"+rate+"%"</span>"+",\<br\>You will receive an amount of "+"<span class='tagged'>"+totalamount+"</span>"+,\<br\>in the year "+"<span class='tagged'>"+year+"</span>"\<br\>
 
 }
 //Slider updates function
